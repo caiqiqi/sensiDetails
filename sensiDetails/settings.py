3a4e82e31657ce91bd22c5af7c271757fce92af8
shadowsock5 <shadowsock5@protonmail.com>
2019-10-10 03:26:08 -0700

    移除不必要的东西

--- sensiDetails/settings.py
+++ sensiDetails/settings.py
@@ -48,7 +48,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'global_user_mail.setting',
             ],
         },
     },
